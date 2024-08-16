e8b891905b981dce3e9dfc7f84bb3b15a448b2b5
Michael Cheung <michaelkawaicheung@yahoo.com.hk>
2024-08-17 02:50:41 +0800

    Add test.

---
+++ tests/test_calculator.py
@@ -0,0 +1,10 @@
+import unittest
+from calculator.calculator import add
+
+class TestCalculator(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(1, 2), 3)
+
+if __name__=='__main__':
+    unittest.main()
