9dc860a36e5453732f7d42b59b886b30e80172f6
Michael Cheung <michaelkawaicheung@yahoo.com.hk>
2024-08-17 03:27:16 +0800

    Add multiply logic.

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,5 @@
 import unittest
-from calculator.calculator import add, subtract
+from calculator.calculator import add, subtract, multiply
 
 class TestCalculator(unittest.TestCase):
 
@@ -8,6 +8,9 @@ class TestCalculator(unittest.TestCase):
 
     def test_subtract(self):
         self.assertEqual(subtract(5, 3), 2)
+
+    def test_multiply(self):
+        self.assertEqual(multiply(3, 4), 12)
 
 if __name__=='__main__':
     unittest.main()
